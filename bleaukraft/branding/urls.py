fb699778faf68bd24d642961c3580fd7c91acb02
ElvisMw <elvismwanthi@gmail.com>
2024-08-06 13:15:39 +0300

    created templates: landingpage, home, services, contacts,aboutus.

---
+++ bleaukraft/branding/urls.py
@@ -0,0 +1,12 @@
+""" branding urls """
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('landingpage/', views.landingpage, name='landingpage'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+]
---
+++ bleaukraft/branding/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'branding/home.html')
+
+def landingpage(request):
+    return render(request, 'branding/landingpage.html')
+
+def about(request):
+    return render(request, 'branding/about.html')
+
+def services(request):
+    return render(request, 'branding/services.html')
+
+def contact(request):
+    return render(request, 'branding/contact.html')
+
+def testimonials(request):
+    return render(request, 'branding/testimonials.html')
+
+def timeline(request):
+    return render(request, 'branding/timeline.html')
