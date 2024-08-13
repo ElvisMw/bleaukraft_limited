d64c824ffd142cbe8d4b6c999f96d13a640a65f9
ElvisMw <elvismwanthi@gmail.com>
2024-08-13 15:02:54 +0300

    landing page update hero section

--- bleaukraft/branding/urls.py
+++ bleaukraft/branding/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
+    path('landingpage1/', views.landingpage1, name='landingpage1'),
+
 ]
