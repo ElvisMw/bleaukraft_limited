069f4b63d627d66f952aa94182867a23a07276de
ElvisMw <elvismwanthi@gmail.com>
2024-09-03 23:06:05 +0300

    update icon before deploy - everything is right. ready to go.

--- bleaukraft/bleaukraft/settings.py
+++ bleaukraft/bleaukraft/settings.py
@@ -112,10 +112,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Where your static files are during development
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic places collected files
 
-STATIC_DIR = BASE_DIR / 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
